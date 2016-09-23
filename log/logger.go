cff9bc891fc5aada1972b4877e42e4a6b6e9197b
yongjin xu <allegiant.xu@hotmail.com>
2016-09-23 17:56:08 +0800

    log

--- log/logger.go
+++ log/logger.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"runtime"
+	"log"
 )
 
 type Slice struct {
@@ -10,13 +11,36 @@ type Slice struct {
 	Y string
 }
 
-func main() {
-	s := Slice{}
-	st := reflect.TypeOf(s)
-	for i := 0; i < st.NumField(); i++ {
-		field := st.Field(i)
-		value := reflect.ValueOf(field)
-		value.SetString("adsfasf")
-		fmt.Println(value.String())
-	}
+func (slice Slice) Ab(str string) {
+	fmt.Println(slice.X + slice.Y)
+	fmt.Println(str)
 }
+
+func info() {
+
+}
+
+
+
+func test() {
+    pc,file,line,ok := runtime.Caller(0)
+    log.Println(pc)
+    log.Println(file)
+    log.Println(line)
+    log.Println(ok)
+    f := runtime.FuncForPC(pc)
+    log.Println(f.Name())
+
+    pc,file,line,ok = runtime.Caller(1)
+    log.Println(pc)
+    log.Println(file)
+    log.Println(line)
+    log.Println(ok)
+    f = runtime.FuncForPC(pc)
+    log.Println(f.Name())
+}
+
+func main() {
+	test()
+}
+
